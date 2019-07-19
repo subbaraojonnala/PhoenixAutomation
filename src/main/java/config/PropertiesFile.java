a8c29d93647bf9bf0da7ff12ef4cdd17ea2da5be
kul_Subbarao <kul_Subbarao@KUL-3D3PXZ1.incontrol-tech.local>
2019-07-19 09:18:21 +0800

    Subba First commit in GIT

---
+++ src/main/java/config/PropertiesFile.java
@@ -0,0 +1,32 @@
+package config;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesFile {
+
+
+	
+	
+	public static  Properties readPropertiesFile() {
+		
+
+		Properties prop = new Properties();
+		try {
+			
+			InputStream input = new FileInputStream("C:\\Users\\kul_subbarao\\git\\PhoenixAutomation\\src\\main\\java\\config\\config.properties");
+		
+			
+			prop.load(input);
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return prop;
+		
+	}
+
+}
